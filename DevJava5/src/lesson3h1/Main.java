9432c3b7dd8ab2e4fa033eafa2d8290fdcc6e150
kuilynad <kuilynad@gmail.com>
2016-11-14 11:59:47 +0200

    commited

--- DevJava5/src/lesson3h1/Main.java
+++ DevJava5/src/lesson3h1/Main.java
@@ -5,9 +5,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-
+		
+		hello();
+		
 		Scanner sc = new Scanner(System.in);
 		//BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	    
@@ -38,6 +40,12 @@ public class Main {
 		}
 		
 		probably();
+		
+		triangle1();
+		
+		square1();
+		
+		triangle2();
 
 	}
 
@@ -81,5 +89,71 @@ public class Main {
 		int i = (int)(Math.random()*101);
 		System.out.println("it's probably true on "+i+"%.");
 	}
+	
+	public static void triangle1() {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("*");
+		}
+	}
+	
+	public static void square1() {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print("*");
+			}
+			System.out.println("*");
+		}
+	}
 
+	public static void triangle2() {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 10; j > i; j--) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public static void hello() {
+		System.out.println("Hello my friends! This is my calculator.");
+	}
+	
+	public static void emptyClass1() {
+		
+	}
+
+	public static void emptyClass2() {
+		
+	}
+
+	public static void emptyClass3() {
+		
+	}
+
+	public static void emptyClass4() {
+		
+	}
+
+	public static void emptyClass5() {
+		
+	}
+
+	public static void emptyClass6() {
+		
+	}
+
+	public static void emptyClass7() {
+		
+	}
+
+	public static void emptyClass8() {
+		
+	}
+
+	public static void emptyClass9() {
+		
+	}
 }
