5ba9c3833a9c0f6882d5b510be482a6eb8b24ca9
kuilynad <kuilynad@gmail.com>
2016-11-16 16:35:50 +0200

    commited

--- DevJava5/src/lesson4h1/Main.java
+++ DevJava5/src/lesson4h1/Main.java
@@ -3,22 +3,101 @@ package lesson4h1;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		task1();
 
+		// task1();
+		// task2();
+		//task3();
+		//task4();
+		//task5();
+		task6();
+		//task7();
+		//task8();
+		//task9();
+		//task10();
 	}
 
-	static void task1() {
-		int[] mas = new int[10];
-		for(int i=0,n=2; i<mas.length; n=n+2,i++){
-			mas[i] = n;
+
+	public static void task1() {
+		int[] array = new int[10];
+		for (int i = 0, n = 2; i < array.length; n = n + 2, i++) {
+			array[i] = n;
 		}
-		for(int i=0; i<mas.length; i++) {
-			System.out.print(mas[i]+" ");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
 		}
 		System.out.println();
-		for(int i=0; i<mas.length; i++) {
-			System.out.println(mas[i]);
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+
+
+	public static void task2() {
+		int[] array = new int[50];
+		for (int i = 0, n = 1; i < array.length; n += 2, i++) {
+			array[i] = n;
+			System.out.print(array[i] + " ");
+		}
+		for (int i = array.length - 1; i >= 0; i--) {
+			System.out.print(array[i] + " ");
+		}
+	}
+
+	
+	public static void task3() {
+		int[] array = new int[15];
+		int j = 0;
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int)(Math.random()*10);
+		}
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] % 2 == 0) {
+				j++;
+			}
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+		System.out.println(j);
+	}
+	
+	public static void task4() {
+		int[] array = new int[8];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int)(Math.random()*10 + 1);
+		}
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < array.length; i++) {
+			if (i%2 == 0) {
+				array[i] = 0;
+			}
+			System.out.print(array[i] + " ");
+		}
+	}
+	
+	public static void task6() {
+		int[] array = new int[4];
+		int flag;
+		flag = 0;
+		for(int i=0; i<array.length; i++){
+			array[i] = (int)(Math.random()*89+10);
+		}
+		for(int i=0; i<array.length; i++) {
+			System.out.print(array[i]+" ");
+		}
+		System.out.println();
+		for(int i=0; i<array.length-1; i++) {
+			if(array[i]>array[i+1]) {
+				flag = 1;
+				break;
+			}
+		}
+		if(flag==1) {
+			System.out.println("NOT strictly increasing sequence");
+		} else {
+			System.out.println("Strictly increasing sequence");
 		}
 	}
 }
