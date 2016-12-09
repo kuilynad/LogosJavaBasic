47d8fab58f3af5a234fe71d9ec93608ec4e40afb
kuilynad <kuilynad@gmail.com>
2016-12-09 15:38:58 +0200

    done commit

--- DevJava5/src/core/less8h/Main.java
+++ DevJava5/src/core/less8h/Main.java
@@ -17,42 +17,48 @@ public class Main {
 	private static final Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		list.add(new Car("Ferarri", new Body("red", "alum"), new Wheel(4, 21, "summer"), new SteeringWheel(5, "black", true)));
-		list.add(new Car("Mclaren", new Body("orange", "alum"), new Wheel(4, 20, "summer"), new SteeringWheel(4, "gray", true)));
-		list.add(new Car("BMW", new Body("yellow", "steel"), new Wheel(4, 18, "summer"), new SteeringWheel(5, "black", false)));
+		list.add(new Car("Ferarri", new Body("red", "alum"), new Wheel(4, 21, "summer"),
+				new SteeringWheel(5, "black", true)));
+		list.add(new Car("Mclaren", new Body("orange", "alum"), new Wheel(4, 20, "summer"),
+				new SteeringWheel(4, "gray", true)));
+		list.add(new Car("BMW", new Body("yellow", "steel"), new Wheel(4, 18, "summer"),
+				new SteeringWheel(5, "black", false)));
 		list.add(new Car("Koenisegg", new Body("white", "carbon"), new Wheel(4, 22, "summer"),
 				new SteeringWheel(3, "white", true)));
-		list.add(new Car("Pagani", new Body("grey", "carbon"), new Wheel(4, 21, "summer"), new SteeringWheel(6, "gray", true)));
-		list.add(new Car("Porshe", new Body("silver", "alum"), new Wheel(4, 19, "summer"), new SteeringWheel(3, "black", false)));
+		list.add(new Car("Pagani", new Body("grey", "carbon"), new Wheel(4, 21, "summer"),
+				new SteeringWheel(6, "gray", true)));
+		list.add(new Car("Porshe", new Body("silver", "alum"), new Wheel(4, 19, "summer"),
+				new SteeringWheel(3, "black", false)));
 		list.add(new Car("Lamborgini", new Body("purple", "alum"), new Wheel(4, 20, "summer"),
 				new SteeringWheel(4, "white", true)));
-		list.add(
-				new Car("Mersedes", new Body("black", "alum"), new Wheel(4, 18, "summer"), new SteeringWheel(3, "brown", false)));
-		list.add(new Car("Nissan", new Body("blue", "steel"), new Wheel(4, 17, "summer"), new SteeringWheel(2, "black", true)));
+		list.add(new Car("Mersedes", new Body("black", "alum"), new Wheel(4, 18, "summer"),
+				new SteeringWheel(3, "brown", false)));
+		list.add(new Car("Nissan", new Body("blue", "steel"), new Wheel(4, 17, "summer"),
+				new SteeringWheel(2, "black", true)));
 		list.add(new Car("Mitsubishi", new Body("green", "steel"), new Wheel(4, 16, "summer"),
 				new SteeringWheel(5, "black", false)));
 
 		while (true) {
 			System.out.println("Enter 1 to find car by body");
-			System.out.println("Enter 2 to ");
-			System.out.println("Enter 3 to ");
-			System.out.println("Enter 4 to ");
-			System.out.println("Enter 5 to ");
+			System.out.println("Enter 2 to delete by color");
+			System.out.println("Enter 3 to change tires type");
+			System.out.println("Enter 4 to delete by body material and size swheel");
+			System.out.println("Enter 5 to remember index");
 			switch (sc.next()) {
 			case "1":
 				task1();
 				break;
 			case "2":
-				// task2();
+				task2();
 				break;
 			case "3":
-				// task3();
+				task3();
 				break;
 			case "4":
-				// task4();
+				task4();
 				break;
 			case "5":
-				// task5();
+				task5();
 				break;
 			default:
 				sc.close();
@@ -72,14 +78,62 @@ public class Main {
 	}
 
 	static void task2() {
-
+		System.out.println("Enter color to delete: ");
+		String col = sc.next();
+		Iterator<Car> iter = list.iterator();
+		while (iter.hasNext()) {
+			Car car = iter.next();
+			if (car.getBody().getColor().toLowerCase().contains(col)) {
+				iter.remove();
+			}
+		}
+		list.forEach(System.out::println);
 	}
 
 	static void task3() {
-		System.out.println();
+		System.out.println("Enter min wheel diameter: ");
+		int min = sc.nextInt();
+		System.out.println("Enter max wheel diameter: ");
+		int max = sc.nextInt();
+		Iterator<Car> iter = list.iterator();
+		while (iter.hasNext()) {
+			Car car = iter.next();
+			if (car.getWheel().getDiameter() > min && car.getWheel().getDiameter() < max) {
+				car.getWheel().setSeason("winter");
+			}
+		}
+		list.forEach(System.out::println);
 	}
 
 	static void task4() {
+		System.out.println("Enter body material: ");
+		String bmat = sc.next();
+		System.out.println("Enter min steering wheel size: ");
+		int smin = sc.nextInt();
+		System.out.println("Enter max steering wheel size: ");
+		int smax = sc.nextInt();
+		Iterator<Car> iter = list.iterator();
+		while (iter.hasNext()) {
+			Car car = iter.next();
+			if ((car.getBody().getMaterial().toLowerCase().contains(bmat)) && (car.getSteeringWheel().getSize() > smin)
+					&& (car.getSteeringWheel().getSize() < smax)) {
+				iter.remove();
+			}
+		}
+		list.forEach(System.out::println);
+	}
 
+	static void task5() {
+		System.out.println("Enter body material");
+		String bmat = sc.next();
+		Iterator<Car> iter = list.iterator();
+		if (!list.isEmpty()) {
+			while (true) {
+				Car car = iter.next();
+				if (car.getBody().getMaterial().equals(bmat)) {
+					System.out.println(list.indexOf(car) + 1);
+				}
+			}
+		}
 	}
 }
